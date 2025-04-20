4e40cd20defc6a9d01457f042c05c6b88ac9b77d
tripleyouuu <emailvitha@gmail.com>
2025-04-20 16:09:50 +0530

    deployment

--- api/news.js
+++ api/news.js
@@ -1,8 +1,8 @@
 export default async function handler(req, res) {
     const API_KEY = process.env.NEWS_API_KEY
-    const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&pageSize=32`, {
+  
+    const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&pageSize=32&apiKey=${API_KEY}`, {
       headers: {
-        "Authorization": `Bearer ${API_KEY}`,
         "User-Agent": "MyNewsApp/1.0"
       }
     })
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,30 +1,13 @@
 import { useEffect, useState } from "react"
 import { ArticleCard } from "../components/ArticleCard"
 
-const API_KEY = import.meta.env.VITE_NEWS_API_KEY;
-const URL = `https://newsapi.org/v2/top-headlines?country=us&pageSize=32&apiKey=${API_KEY}`;
-
-
 export function Home() {
   const [articles, setArticles] = useState([])
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(false)
 
-  // useEffect(() => {
-  //   fetch(URL)
-  //     .then((res) => res.json())
-  //     .then((data) => {
-  //       setArticles(data.articles || [])
-  //       setLoading(false)
-  //     })
-  //     .catch(() => {
-  //       setError(true)
-  //       setLoading(false)
-  //     })
-  // }, [])
-
   useEffect(() => {
-    fetch('/api/news')
+    fetch("/api/news")
       .then((res) => res.json())
       .then((data) => {
         setArticles(data.articles || [])
@@ -35,19 +18,16 @@ export function Home() {
         setLoading(false)
       })
   }, [])
-  
 
   if (loading) return <p className="text-center">Loading top headlines...</p>
   if (error) return <p className="text-center text-red-600">Failed to load news.</p>
 
   return (
     <div>
-    <center><h1 className="text-2xl font-bold">Top Headlines</h1></center>
-      <div className="article-grid">
+      <center><h1 className="text-2xl font-bold">Top Headlines</h1></center>
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 px-4 py-6">
         {articles.map((article) => (
-          <div key={article.url} className="article-card">
-            <ArticleCard article={article} />
-          </div>
+          <ArticleCard key={article.url} article={article} />
         ))}
       </div>
     </div>
